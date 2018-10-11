a8f83a297c6256dbf43c0e30b5bd696bfd379443
david.greatorex <david.greatorex@digital.hmrc.gov.uk>
2018-10-11 15:02:47 +0100

    SCRS-10853 Update of page titles and URIs and dependency uplift

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -24,14 +24,14 @@ private object AppDependencies {
   private val playReactivemongoVersion = "6.2.0"
   private val playConditionalFormMappingVersion = "0.2.0"
   private val playLanguageVersion = "3.4.0"
-  private val bootstrapVersion = "3.6.0"
+  private val bootstrapVersion = "3.9.0"
   private val scalacheckVersion = "1.13.4"
   private val jsoupVersion = "1.11.2"
   private val scoverageVersion = "1.3.1"
   private val wireMockVersion = "2.6.0"
   private val reactivemongoTestVersion = "3.1.0"
   private val playWhitelistVersion     = "2.0.0"
-  private val frontendBootstrapVersion = "10.3.0"
+  private val frontendBootstrapVersion = "10.5.0"
 
   val compile = Seq(
     ws,
