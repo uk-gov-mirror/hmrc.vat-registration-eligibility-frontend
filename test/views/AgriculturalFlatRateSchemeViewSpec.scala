9928ded5ad7a058e985322cd63c3fcf2ea6ce5b3
Brenna Samuel <42062758+brennaksamuel@users.noreply.github.com>
2020-09-03 14:45:14 +0100

    SAR-6130: Updated viewspec for AFRS

--- test/views/AgriculturalFlatRateSchemeViewSpec.scala
+++ test/views/AgriculturalFlatRateSchemeViewSpec.scala
@@ -17,26 +17,55 @@
 package views
 
 import controllers.routes
-import deprecated.DeprecatedConstants
 import forms.AgriculturalFlatRateSchemeFormProvider
 import models.NormalMode
 import play.api.data.Form
-import views.newbehaviours.YesNoViewBehaviours
+import play.twirl.api.HtmlFormat
 import views.html.agriculturalFlatRateScheme
+import views.newbehaviours.YesNoViewBehaviours
 
 class AgriculturalFlatRateSchemeViewSpec extends YesNoViewBehaviours {
-  val extraParamForLegend: String = DeprecatedConstants.fakeCompanyName
+
   val messageKeyPrefix = "agriculturalFlatRateScheme"
   val form = new AgriculturalFlatRateSchemeFormProvider()()
   implicit val msgs = messages
 
-  def createView = () => agriculturalFlatRateScheme(form, NormalMode)(fakeDataRequestIncorped, messages, frontendAppConfig)
+  val saveAndContinueButton = "Save and continue"
+  val h1 = "Is the business applying for the Agricultural Flat Rate Scheme?"
+  val p1 = "The Agricultural Flat Rate Scheme is an alternative to VAT registration for farmers."
+  val afrsLinkText1 = "Find out more about the"
+  val afrsLinkText2 = "Agricultural Flat Rate Scheme (opens in a new window)."
+  val afrsLink = "https://www.gov.uk/government/publications/vat-notice-70046-agricultural-flat-rate-scheme/vat-notice-70046-agricultural-flat-rate-scheme"
+  val p2 = s"$afrsLinkText1 $afrsLinkText2"
 
-  def createViewUsingForm = (form: Form[_]) => agriculturalFlatRateScheme(form, NormalMode)(fakeDataRequestIncorped, messages, frontendAppConfig)
+  object Selectors extends BaseSelectors
+
+
+  def createView: () => HtmlFormat.Appendable =
+    () => agriculturalFlatRateScheme(form, NormalMode)(fakeDataRequest, messages, frontendAppConfig)
+
+  def createViewUsingForm: Form[_] => HtmlFormat.Appendable =
+    (form: Form[_]) => agriculturalFlatRateScheme(form, NormalMode)(fakeDataRequest, messages, frontendAppConfig)
 
   "AgriculturalFlatRateScheme view" must {
-    behave like normalPage(createView(), messageKeyPrefix, Seq(DeprecatedConstants.fakeCompanyName))
-
+    behave like normalPage(createView(), messageKeyPrefix)
+    behave like pageWithBackLink(createViewUsingForm(form))
     behave like yesNoPage(form, createViewUsingForm, messageKeyPrefix, routes.AgriculturalFlatRateSchemeController.onSubmit().url)
+    behave like pageWithSubmitButton(createViewUsingForm(form), saveAndContinueButton)
+  }
+
+  "have the correct heading" in {
+    val doc = asDocument(createViewUsingForm(form))
+    doc.select(Selectors.h1).text() mustBe h1
+  }
+
+  "have the first paragraph" in {
+    val doc = asDocument(createViewUsingForm(form))
+    doc.select(Selectors.p(1)).text() mustBe p1
+  }
+
+  "have the second paragraph with the correct url link" in {
+    val doc = asDocument(createViewUsingForm(form))
+    doc.select(Selectors.p(2)).select("a").attr("href") mustBe afrsLink
   }
 }
--- test/views/VoluntaryRegistrationViewSpec.scala
+++ test/views/VoluntaryRegistrationViewSpec.scala
@@ -20,8 +20,9 @@ import controllers.routes
 import forms.VoluntaryRegistrationFormProvider
 import models.NormalMode
 import play.api.data.Form
-import views.newbehaviours.YesNoViewBehaviours
+import play.twirl.api.HtmlFormat
 import views.html.voluntaryRegistration
+import views.newbehaviours.YesNoViewBehaviours
 
 class VoluntaryRegistrationViewSpec extends YesNoViewBehaviours {
 
@@ -40,19 +41,11 @@ class VoluntaryRegistrationViewSpec extends YesNoViewBehaviours {
 
   object Selectors extends BaseSelectors
 
-  val expectedElements = Seq(
-    Selectors.h1 -> h1,
-    Selectors.p(1) -> paragraph,
-    Selectors.bullet(1) -> bullet1,
-    Selectors.bullet(2) -> bullet2,
-    Selectors.bullet(3) -> bullet3,
-    Selectors.h2(1) -> h2,
-    Selectors.indent -> indentText
-  )
+  def createView: () => HtmlFormat.Appendable =
+    () => voluntaryRegistration(form, NormalMode)(fakeDataRequest, messages, frontendAppConfig)
 
-  def createView = () => voluntaryRegistration(form, NormalMode)(fakeDataRequestIncorped, messages, frontendAppConfig)
-
-  def createViewUsingForm = (form: Form[_]) => voluntaryRegistration(form, NormalMode)(fakeDataRequestIncorped, messages, frontendAppConfig)
+  def createViewUsingForm: Form[_] => HtmlFormat.Appendable =
+    (form: Form[_]) => voluntaryRegistration(form, NormalMode)(fakeDataRequest, messages, frontendAppConfig)
 
   "VoluntaryRegistration view" must {
     behave like normalPage(createView(), messageKeyPrefix)
